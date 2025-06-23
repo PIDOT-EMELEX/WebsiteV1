a03907e87df4e5f9a1e012fe7c9bc3884b8a20b2
PIDOT-EMELEX <vamshi@pidot.in>
2025-06-23 18:02:12 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def axiom(request):
-    return render(request, 'products\product_axiom.html')
+    return render(request, 'products/product_axiom.html')
 
 def edusphere(request):
-    return render(request, 'products\product_edusphere.html')
+    return render(request, 'products/product_edusphere.html')
 
 def genesis(request):
-    return render(request, 'products\product_genesis.html')
+    return render(request, 'products/product_genesis.html')
 
 def solutions(request):
-    return render(request, 'products\solutions_all.html')
+    return render(request, 'products/solutions_all.html')
 
 def Genesis_stu_reg(request):
-    return render(request, 'products\Genesis_stu_reg.html')
+    return render(request, 'products/product_genesis.html')
